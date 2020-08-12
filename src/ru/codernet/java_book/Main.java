5e4539d997910f41e08db2b6707a7fe03ff75153
Elena <helen_perets@mail.ru>
2020-08-12 09:48:15 +0300

    Initial commit

---
+++ src/ru/codernet/java_book/Main.java
@@ -0,0 +1,10 @@
+package ru.codernet.java_book;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        int c = 6+7;
+        System.out.println(" Значение с равно " + c);
+    }
+}
